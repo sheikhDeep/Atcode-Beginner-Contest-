a2473794ac4b0e343280d7ba03e57929e4eca678
Sheikh Deep <30627010+sheikhDeep@users.noreply.github.com>
2019-03-29 03:00:52 +0600

    Add files via upload

---
+++ Atcoder Beginner 122C.cpp
@@ -0,0 +1,87 @@
+#include <bits/stdc++.h>
+
+
+using namespace std;
+
+#define endl '\n'
+#define PB push_back
+#define MP make_pair
+#define LL long long
+#define FOR(i,a,b) for(int i = (a); i <= (b); i++)
+#define RE(i,n) FOR(i,1,n)
+#define REP(i,n) FOR(i,0,(int)(n)-1)
+#define R(i,n) REP(i,n)
+#define VI vector<int>
+#define PII pair<int,int>
+#define LD long double
+#define FI first
+#define SE second
+#define st FI
+#define nd SE
+#define ALL(x) (x).begin(), (x).end()
+#define SZ(x) ((int)(x).size())
+
+typedef long long ll;
+
+bool file=0,rt=1;
+clock_t tStart ;
+void FAST_IO();
+////////////////////////
+
+int main() {
+
+    //FAST_IO();
+    ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout << setprecision(5);
+    cout << fixed;
+
+    int n, q;
+    string s;
+
+    cin >> n >> q;
+    cin >> s;
+
+    int print[n];
+    print[0] = 0;
+    for(int i = 1; i < n; i++) {
+
+        if(s[i-1] == 'A' && s[i] == 'C') {
+
+            print[i] = print[i-1] + 1;
+        }
+        else print[i] = print[i-1];
+    }
+
+    while(q--) {
+
+        int l, r;
+
+        cin >> l >> r;
+
+
+
+        cout << print[r-1] - print[l-1] << endl;
+    }
+
+
+    return 0;
+}
+
+void FAST_IO()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    //cout.tie(NULL);
+    //cout.setf(ios::fixed);
+    //cout.precision(2);
+    if(rt && file){ tStart = clock(); }
+    if(file){
+    #ifndef _offline
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+    }
+}
+
+
