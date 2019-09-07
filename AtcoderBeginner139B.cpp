4395b3ebdd7d9acd75bd5f730f2410056ae43499
Sheikh Deep <30627010+sheikhDeep@users.noreply.github.com>
2019-09-07 22:14:15 +0600

    Add files via upload

---
+++ AtcoderBeginner139B.cpp
@@ -0,0 +1,47 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+#define EPS (1e-7)
+#define INF (1e9)
+#define PI (acos(-1))
+
+#define long long int ll
+#define endl '\n'
+
+
+int dx[4] = {1, 0, -1, 0}, dy[4] = {0, 1, 0, -1};
+
+
+const int MOD = 1000000007;
+
+
+
+
+
+int main() {
+
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+
+    int a, b;
+
+    cin >> a >> b;
+
+    int tot = 1;
+
+    int ans = 1;
+
+
+    while(tot < b) {
+
+        tot += a - 1;
+        ans++;
+    }
+
+    cout << ans << endl;
+
+
+
+
+}
+
