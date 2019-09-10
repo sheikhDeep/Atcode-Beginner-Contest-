854dd097070b880a31be201c30113e10318bcf9e
Sheikh Deep <30627010+sheikhDeep@users.noreply.github.com>
2019-09-10 20:11:52 +0600

    Add files via upload

---
+++ AtcoderBeginner140A.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+#define EPS (1e-7)
+#define INF (1e9)
+#define PI (acos(-1))
+
+typedef long long  ll;
+#define endl '\n'
+
+
+int dx[4] = {1, 0, -1, 0}, dy[4] = {0, 1, 0, -1};
+
+
+const int MOD = 1000000007;
+
+int pre[9];
+
+int main() {
+
+  cin.tie(0);
+  ios::sync_with_stdio(false);
+
+  int n;
+
+  cin >> n;
+
+  cout << n * n * n << endl;
+
+
+
+
+}
+
